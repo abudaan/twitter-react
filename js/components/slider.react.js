2d2505ad766c0976f940baaf8a4babbff97e4675
abudaan <daniel@abumarkub.net>
2015-12-07 14:09:51 +0100

    animations

---
+++ js/components/slider.react.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import guid from '../util/guid';
+
+class Slider extends React.Component{
+
+  static displayName = 'Slider';
+
+  constructor(props){
+    super(props);
+  }
+
+
+  render(){
+
+    let result = [];
+    let tweets = this.props.tweets;
+
+    tweets.forEach(function(tweet){
+
+      let user_name = `@${tweet.user_name}`;
+      let user_url = `https://twitter.com/${tweet.user_name}`;
+      let tweet_url = `https://twitter.com/${tweet.user_name}/status/${tweet.id}`;
+
+      function createHTML(){
+        if(tweet.retweet){
+          return {__html: `<a class="retweet" target="blank" href="${tweet_url}">&#61561;</a>${tweet.text}`};
+        }
+        return {__html: tweet.text};
+      }
+
+
+      if(tweet.media_url){
+        result.push(
+          <div className="slide" key={tweet.id}>
+            <div className="flexbox">
+              <a href={user_url} target="blank"><div className="user"><img src={tweet.user_img} /><span>{user_name}</span></div></a>
+              <p className="tweet" dangerouslySetInnerHTML={createHTML()} />
+              <a className="media" href={tweet_url} target="blank"><img src={tweet.media_url} /></a>
+            </div>
+          </div>
+        );
+      }else{
+        result.push(
+          <div className="slide" key={tweet.id}>
+            <div className="flexbox">
+              <a href={user_url} target="blank"><div className="user"><img src={tweet.user_img} /><span>{user_name}</span></div></a>
+              <p className="tweet" dangerouslySetInnerHTML={createHTML()} />
+            </div>
+          </div>
+        );
+      }
+    });
+
+    return (
+        <div key={guid()} className="slider">
+          {result}
+        </div>
+    );
+  }
+}
+
+Slider.propTypes = {
+  tweets: React.PropTypes.array
+};
+
+export default Slider;
--- js/components/tweets.react.js
+++ js/components/tweets.react.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Slider from '../components/slider.react.js';
+import guid from '../util/guid.js';
 let ReactTransitionGroup = require('react-addons-css-transition-group');
+let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 
 class Tweets extends React.Component{
@@ -8,58 +11,21 @@ class Tweets extends React.Component{
 
   constructor(props){
     super(props);
-
   }
 
   render(){
-    let result = [];
-    let tweets = this.props.tweets;
-    tweets.forEach(function(tweet){
-
-      let user_name = `@${tweet.user_name}`;
-      let user_url = `https://twitter.com/${tweet.user_name}`;
-      let tweet_url = `https://twitter.com/${tweet.user_name}/status/${tweet.id}`;
-
-      function createHTML(){
-        if(tweet.retweet){
-          return {__html: `<a class="retweet" target="blank" href="${tweet_url}">&#61561;</a>${tweet.text}`};
-        }
-        return {__html: tweet.text};
-      }
-
-
-      if(tweet.media_url){
-        result.push(
-          <div className="slide" key={tweet.id}>
-            <div className="flexbox">
-              <a href={user_url} target="blank"><div className="user"><img src={tweet.user_img} /><span>{user_name}</span></div></a>
-              <p className="tweet" dangerouslySetInnerHTML={createHTML()} />
-              <a href={tweet_url} target="blank" className="media"><img src={tweet.media_url} /></a>
-            </div>
-          </div>
-        );
-      }else{
-        result.push(
-          <div className="slide" key={tweet.id}>
-            <div className="flexbox">
-              <a href={user_url} target="blank"><div className="user"><img src={tweet.user_img} /><span>{user_name}</span></div></a>
-              <p className="tweet" dangerouslySetInnerHTML={createHTML()} />
-            </div>
-          </div>
-        );
-      }
-    });
-    // return (
-    //   <div className="slider">
-    //     <ReactTransitionGroup key="transition" component="div" transitionAppear={true} transitionName="example" transitionAppearTimeout={0} transitionLeaveTimeout={0}>
-    //       {result}
-    //     </ReactTransitionGroup>
-    //   </div>
-    // );
     return (
-      <div className="slider">
-        {result}
-      </div>
+      <ReactCSSTransitionGroup
+          key="transition"
+          component="div"
+          // transitionAppear={true}
+          transitionName="slider"
+          // transitionAppearTimeout={0}
+          transitionEnterTimeout={0}
+          transitionLeaveTimeout={0}
+        >
+        <Slider tweets={this.props.tweets} key={guid()} />
+      </ReactCSSTransitionGroup>
     );
   }
 }
